04cd651aff31b44a4c0bcba8ccf5a0082baf962e
pahariyamohit9@gmail.com <pahariyamohit9@gmail.com>
2023-08-25 05:12:37 +0530

    Initial commit

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	// $conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('sql111.infinityfree.com','if0_34771519','93139010129010','if0_34771519_test');// (sql localhost, username, pass,database name)
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
